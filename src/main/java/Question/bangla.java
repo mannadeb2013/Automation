a6c61d590e227f499a7a880edde9a57d0540060f
manna <manna@10.0.0.115>
2022-10-06 15:23:44 -0400

    comit 4

---
+++ src/main/java/Question/bangla.java
@@ -0,0 +1,18 @@
+package Question;
+
+public class bangla {
+	
+	public void kola() {
+		int a = 20;
+		int b = 30;
+		int c;
+		c=a;
+		a=b;
+		System.out.println(a);
+		
+	}
+public static void main(String[] args) {
+	bangla obj = new bangla();
+	obj.kola();
+}
+}
